bb36e5569fd936281901e5db6d863aed910784c9
erackley <erackley@malwarebtes.org>
2014-01-26 14:51:39 -0500

    Final Scene.

--- Assets/Scripts/ObjectMovement/FollowCameraScript.cs
+++ Assets/Scripts/ObjectMovement/FollowCameraScript.cs
@@ -5,14 +5,23 @@ public class FollowCameraScript : MonoBehaviour{
 
 	public Transform myCameraTransform;
 	public float myFollowSpeed = 1.0f;
-	
+	public bool Enabled = true;
+
+	void EnableFollow()
+	{
+		Enabled = true;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = this.transform.position;
-		pos = Vector3.MoveTowards (pos, myCameraTransform.position, myFollowSpeed * Time.fixedDeltaTime);
-		//pos.x = Mathf.Lerp(pos.x, myCameraTransform.position.x, myFollowSpeed * Time.deltaTime);
-		//pos.y = Mathf.Lerp(pos.y, myCameraTransform.position.y, myFollowSpeed * Time.deltaTime);		
-		pos.z = this.transform.position.z;
-		this.transform.position = pos;	
+		if (Enabled == true) 
+		{
+			Vector3 pos = this.transform.position;
+			pos = Vector3.MoveTowards (pos, myCameraTransform.position, myFollowSpeed * Time.fixedDeltaTime);
+						//pos.x = Mathf.Lerp(pos.x, myCameraTransform.position.x, myFollowSpeed * Time.deltaTime);
+						//pos.y = Mathf.Lerp(pos.y, myCameraTransform.position.y, myFollowSpeed * Time.deltaTime);		
+			pos.z = this.transform.position.z;
+			this.transform.position = pos;	
+		}
 	}
 }
--- Assets/Scripts/VictoryCondition.cs
+++ Assets/Scripts/VictoryCondition.cs
@@ -15,7 +15,7 @@ public class VictoryCondition : MonoBehaviour {
 		{
 			m_moveWorld.RotateCameraTo(180);
 			Camera.main.backgroundColor = new Color(0,136/255.0f,164/255.0f,1);
-			
+			BroadcastMessage("EnableFollow");
 		}
 	}
 	
