4996ecb7b68f2af45c15c014203c50f3ea00e080
erackley <erackley@malwarebtes.org>
2014-01-25 20:12:40 -0500

    Adding music etc.

---
+++ Assets/sound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+public class sound : MonoBehaviour {
+
+	private bool m_hearingSound = false;
+	private float m_soundVolume = 0;
+	private float m_maxSoundVolume = 0;
+	private AudioSource m_AudioSrc;
+	// Use this for initialization
+	void Start () {
+		m_AudioSrc = GetComponent<AudioSource>();
+	
+	}
+
+	public void increaseVolume(float maxSoundVolume) {
+		m_maxSoundVolume = maxSoundVolume;
+		m_hearingSound = true;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	void FixedUpdate() {
+		if ( m_hearingSound == true ) {
+			if (m_AudioSrc.volume < 1.0f) {
+				m_AudioSrc.volume += .01f;
+			}
+		}
+	}
+}
