9de82856338d9e70915585aa18303e4df0c0e921
erackley <erackley@malwarebtes.org>
2014-01-25 23:32:30 -0500

    Unless you have meta data...

---
+++ Assets/Scripts/WeightedGateScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightedGateScript : MonoBehaviour 
+{
+	public CircleCollider2D ColliderA;
+	public CircleCollider2D ColliderB;
+	public CircleCollider2D ColliderC;
+	public CircleCollider2D ColliderD;
+
+	Start()
+	{
+	}
+
+	void OnCollisionStay(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player") 
+		{
+		}
+	}
+
+
+}
