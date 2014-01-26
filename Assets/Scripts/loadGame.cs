f9d83213459eead00e19dec3581e20a7053be077
erackley <erackley@malwarebtes.org>
2014-01-26 15:22:36 -0500

    Final changes for Game Jam 2014!!!

--- Assets/Scripts/loadGame.cs
+++ Assets/Scripts/loadGame.cs
@@ -11,7 +11,7 @@ public class loadGame : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			Application.LoadLevel("TestSoundAdded");
+			Application.LoadLevel("EaganTest2");
 		}
 	
 	}
