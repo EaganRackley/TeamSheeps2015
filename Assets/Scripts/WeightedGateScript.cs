8f756c951fe1439226af1c591bfa72c8ce520808
erackley <erackley@malwarebtes.org>
2014-01-26 01:09:06 -0500

    Added WeightedGate and Paralllax effects to EaganTest.

--- Assets/Scripts/WeightedGateScript.cs
+++ Assets/Scripts/WeightedGateScript.cs
@@ -7,15 +7,55 @@ public class WeightedGateScript : MonoBehaviour
 	public CircleCollider2D ColliderB;
 	public CircleCollider2D ColliderC;
 	public CircleCollider2D ColliderD;
+	public PlayerController PlayerObject;
 
-	Start()
+	private bool m_Triggered = false;
+
+	void Start()
 	{
+		m_Triggered = false;
 	}
 
-	void OnCollisionStay(Collider collider)
+	void OnTriggerEnter2D(Collider2D collider)
 	{
 		if (collider.gameObject.tag == "Player") 
 		{
+			m_Triggered = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player") 
+		{
+			m_Triggered = false;
+		}
+	}
+
+	void Update()
+	{
+		if (m_Triggered) 
+		{
+			print ("Player Scale: " + PlayerObject.transform.localScale.x);
+			Animator animator = GetComponent<Animator>();
+			if(PlayerObject.transform.localScale.x >= 1.2f && PlayerObject.transform.localScale.x < 1.4f)
+			{
+				animator.SetBool("Wobble", true);
+			}
+			else if(PlayerObject.transform.localScale.x >= 1.4f)
+			{
+				animator.SetBool("Wobble", false);
+				animator.SetBool("Collapsed", true);
+				ColliderA.isTrigger = true;
+				ColliderB.isTrigger = true;
+				ColliderC.isTrigger = true;
+				ColliderD.isTrigger = true;		
+			}
+			else if(PlayerObject.transform.localScale.x >= 0.2f)
+			{
+				animator.SetBool("Collapsed", false);
+				animator.SetBool("Wobble", false);
+			}
 		}
 	}
 
