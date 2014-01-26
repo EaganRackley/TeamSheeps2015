c4fc41990288631394071ec1cf8b67c6311921d5
Grant Gardiner <Gardiner.Grant@gmail.com>
2014-01-25 18:09:05 -0800

    made modifications to music zone and sound.cs deleted movecube as we don't use/need it.

--- Assets/Scripts/MusicZone.cs
+++ Assets/Scripts/MusicZone.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class MusicZone : MonoBehaviour {
-
+	public sound music;
+	
 	// Use this for initialization
 	void Start () {
+		
+	}
 	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player" )
+		{
+			music.increaseVolume(1.0f);			
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
