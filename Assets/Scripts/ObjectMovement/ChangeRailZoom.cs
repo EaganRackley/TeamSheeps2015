beb757cada168d09be12828da58df802f2c1dda6
erackley <erackley@malwarebtes.org>
2014-02-03 23:58:37 -0500

    * Added missing scripts and meta data.

---
+++ Assets/Scripts/ObjectMovement/ChangeRailZoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeRailZoom : MonoBehaviour {
+
+	public float NewMaxSpeed = 1.0f;
+	public float NewMaxCameraSize = 48.0f;
+	public float NewCameraSizeSpeed = 10.0f;
+	public RailCart RailCartToModify;
+	private bool m_Modified = false;
+
+	// Use this for initialization
+	void Start () {
+		m_Modified = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player" && m_Modified == false) 
+		{
+			RailCartToModify.ResetRailcartSettings( NewMaxSpeed, NewMaxCameraSize, NewCameraSizeSpeed );
+			m_Modified = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
