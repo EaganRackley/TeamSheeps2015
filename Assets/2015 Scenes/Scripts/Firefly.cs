ced0a5ddd3994fa93e9037a6b4bea334863d38b5
Paul Buser <pbuser@oberlin.edu>
2015-01-24 18:23:28 -0500

    Add fireflies.

---
+++ Assets/2015 Scenes/Scripts/Firefly.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firefly : MonoBehaviour {
+
+    public float onTime;
+    public float offTime;
+    public float randomVariance;
+
+    float lightCountdown;
+    bool lightOn;
+
+    Light fireflyLight;
+
+    void Awake()
+    {
+        fireflyLight = GetComponentInChildren<Light>();
+    }
+
+    void Start()
+    {
+        if (Random.Range(0, 1) > 0)
+        {
+            this.lightOn = true;
+            lightCountdown = onTime + Random.Range(0f, randomVariance);
+        }
+        else
+        {
+            this.lightOn = false;
+            lightCountdown = offTime + Random.Range(0f, randomVariance);
+        }
+    }
+
+    void Update()
+    {
+        lightCountdown -= Time.deltaTime;
+        if (lightCountdown <= 0f)
+        {
+            if (this.lightOn)
+            {
+                this.lightOn = false;
+                lightCountdown = offTime + Random.Range(0f, randomVariance);
+            }
+            else
+            {
+                this.lightOn = true;
+                lightCountdown = onTime + Random.Range(0f, randomVariance);
+            }
+        }
+        UpdateLight();
+    }
+
+    void UpdateLight()
+    {
+        if (this.lightOn)
+            fireflyLight.enabled = true;
+        else
+            fireflyLight.enabled = false;
+    }
+}
