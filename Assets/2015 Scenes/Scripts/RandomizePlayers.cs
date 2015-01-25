2fb59e566c63f2bffe618a35b43d26940981a920
Paul Buser <pbuser@oberlin.edu>
2015-01-25 13:30:42 -0500

    Added randomization of player gender and hair color.

---
+++ Assets/2015 Scenes/Scripts/RandomizePlayers.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizePlayers : MonoBehaviour {
+
+    public RuntimeAnimatorController lightHairMale;
+    public RuntimeAnimatorController lightHairFemale;
+    public RuntimeAnimatorController darkHairMale;
+    public RuntimeAnimatorController darkHairFemale;
+
+    void Awake()
+    {
+        GameObject player1 = GameObject.FindGameObjectWithTag("Player");
+        GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
+        // Maybe swap which player is which
+        if (Random.value >= 0.5f)
+        {
+            GameObject buffer = player2;
+            player2 = player1;
+            player1 = buffer;
+        }
+        Animator player1Anim = player1.GetComponent<Animator>();
+        Animator player2Anim = player2.GetComponent<Animator>();
+        PlayerController p1Control = player1.GetComponent<PlayerController>();
+        PlayerController p2Control = player2.GetComponent<PlayerController>();
+
+        // Randomize player1
+        if (Random.value >= 0.5f)
+        {
+            player1Anim.runtimeAnimatorController = lightHairMale;
+            p1Control.gender = PlayerController.Gender.M;
+        }
+        else
+        {
+            player1Anim.runtimeAnimatorController = lightHairFemale;
+            p1Control.gender = PlayerController.Gender.F;
+        }
+
+        // Randomize player2
+        if (Random.value >= 0.5f)
+        {
+            player2Anim.runtimeAnimatorController = darkHairMale;
+            p2Control.gender = PlayerController.Gender.M;
+        }
+        else
+        {
+            player2Anim.runtimeAnimatorController = darkHairFemale;
+            p2Control.gender = PlayerController.Gender.F;
+        }
+    }
+}
