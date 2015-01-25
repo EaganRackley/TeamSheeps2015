cae77610c8b08d1b8d6d31d2b34e3dc22ef1316e
Paul Buser <pbuser@oberlin.edu>
2015-01-25 00:27:21 -0500

    Made EventManager to spawn fireflies.

---
+++ Assets/2015 Scenes/Scripts/EventManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour 
+{
+    Transform topLeft;
+    Transform bottomRight;
+
+    public GameObject fireflyPrefab;
+
+    void Awake()
+    {
+        this.topLeft = GameObject.FindGameObjectWithTag("TopLeft").transform;
+        this.bottomRight = GameObject.FindGameObjectWithTag("BottomRight").transform;
+    }
+
+    IEnumerator SpawnFireFlies(int numberOfFireFlies)
+    {
+            for (int i  = 0; i < numberOfFireFlies; i++)
+            {
+            Random.seed = i;
+            Vector3 spawnPosition = new Vector3(Random.Range(topLeft.transform.position.x, bottomRight.transform.position.x),
+                                                Random.Range(bottomRight.transform.position.y, topLeft.transform.position.y),
+                                                -1f);
+            Debug.Log("Spawning at " + spawnPosition);
+            // Don't spawn if the firefly would spawn in view of the camera
+            Plane[] frustum = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+            if(!GeometryUtility.TestPlanesAABB(frustum, new Bounds(spawnPosition, Vector3.one)))
+            {
+                Instantiate(fireflyPrefab, spawnPosition, Quaternion.identity);
+            }
+            yield return null;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(SpawnFireFlies(100));
+        }
+    }
+}
