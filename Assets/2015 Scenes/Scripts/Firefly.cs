7b4b49126d616b069a44001642c9b7d481ea0fb7
Paul Buser <pbuser@oberlin.edu>
2015-01-24 19:44:51 -0500

    Added fireflies. They good now!

--- Assets/2015 Scenes/Scripts/Firefly.cs
+++ Assets/2015 Scenes/Scripts/Firefly.cs
@@ -7,6 +7,23 @@ public class Firefly : MonoBehaviour {
     public float offTime;
     public float randomVariance;
 
+    public float moveSpeed;
+
+    public float timePerTurningDirection;
+    public float randomTimePerTurningDirection;
+    public float turnSpeed;
+    public float randomTurnSpeed;
+    bool turningRight = false;
+
+    float turningTimer;
+
+    const float UPDOWN_RANGE = 0.5f;
+    const float SECONDS_PER_CYCLE = 5f;
+    float updownTimer;
+    float initialHeight;
+
+    float currentDirection = 0f;
+
     float lightCountdown;
     bool lightOn;
 
@@ -15,6 +32,7 @@ public class Firefly : MonoBehaviour {
     void Awake()
     {
         fireflyLight = GetComponentInChildren<Light>();
+        initialHeight = transform.position.z;
     }
 
     void Start()
@@ -48,6 +66,29 @@ public class Firefly : MonoBehaviour {
             }
         }
         UpdateLight();
+        MoveUpDown();
+        MoveAround();
+    }
+
+    void MoveAround()
+    {
+        this.turningTimer -= Time.deltaTime;
+        if (turningTimer <= 0f)
+        {
+            this.turningTimer = timePerTurningDirection + Random.Range(0f, randomTimePerTurningDirection);
+            this.turningRight = !this.turningRight;
+        }
+        if (this.turningRight)
+        {
+            currentDirection -= turnSpeed + Random.Range(0f, randomTurnSpeed);
+        }
+        else
+        {
+            currentDirection += turnSpeed + Random.Range(0f, randomTurnSpeed);
+        }
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentDirection);
+        Vector2 movementVector = new Vector2(Mathf.Cos(currentDirection * Mathf.Deg2Rad), Mathf.Sin(currentDirection * Mathf.Deg2Rad));
+        transform.position += new Vector3(movementVector.x, movementVector.y, 0f) * moveSpeed * Time.deltaTime;
     }
 
     void UpdateLight()
@@ -56,5 +97,13 @@ public class Firefly : MonoBehaviour {
             fireflyLight.enabled = true;
         else
             fireflyLight.enabled = false;
+    }
+
+    void MoveUpDown()
+    {
+        updownTimer += Time.deltaTime * (1f / SECONDS_PER_CYCLE);
+        if (updownTimer > 1f)
+            updownTimer -= 1f;
+        transform.position = new Vector3(transform.position.x, transform.position.y, initialHeight + Mathf.Sin(updownTimer * 2 * Mathf.PI) * UPDOWN_RANGE);
     }
 }
