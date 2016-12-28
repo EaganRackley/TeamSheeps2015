8fdda756fcf6fac7de121fd639cb823ce5659a39
erackley <erackley@malwarebytes.org>
2016-12-28 12:51:52 -0500

    * Added script that will destroy palette objects for non-player prefab.

---
+++ Assets/GridsInteractiveArt/Scripts/PlayerPaintPaletteHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerPaintPaletteHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NetworkBehaviour parentNB = GetComponentInParent<NetworkBehaviour>();
+        if(parentNB != null)
+        {
+            if(!parentNB.isLocalPlayer)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
