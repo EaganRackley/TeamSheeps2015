554fbee1aa9015677b341e24620d86ecf63fe853
erackley <erackley@malwarebytes.com>
2020-03-15 21:58:01 -0400

    First start as level with tilemap design - added clear water as a questionable choice - child tilemaps that are a sub-size of master tilemaps could allow for some animated decorations.

--- Assets/Scripts/FallingBlockScript.cs
+++ Assets/Scripts/FallingBlockScript.cs
@@ -1,84 +1,89 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FallingBlockScript : MonoBehaviour {
-
-    public float Lifetime = 300.0f;
-    public float DestroyAfter = 310.0f;
+public class FallingBlockScript : MonoBehaviour
+{
+    private float m_lifetime = 300.0f;
+    private float m_destroyAfter = 310.0f;
     public float ShakeTime = 2.0f;
     public float MinShakeMagnitude = 0.01f;
     public float MaxShakeMagnitude = 0.1f;
-    public float MaxFallOffset = 5.0f;
+    private float m_maxFallOffset = 5.0f;
+    public float FallTimeStart = 60.0f;
 
     private float m_LifeSpent = 0.0f;
     private float m_CurrentShakeMag = 0.0f;
     private Vector3 m_StartPosition;
 
-	private const float FALL_TIME_MULTIPLIER = 1.9f;
-	private const float FALL_TIME_START = 60.0f;
+    private const float FallTimeMultiplier = 1.9f;
 
     void Start()
     {
-		m_LifeSpent = 0.0f;
-        float rightSideSecondOffset = 62;
+        m_LifeSpent = 0f;
+
         // Set Lifetime and Destroy After based on X position
         Vector3 worldPosition = this.transform.position;
-        if (worldPosition.x <= 24)
+
+        if (worldPosition.x <= 0)
         {
-            worldPosition.x += 36;
-			Lifetime = FALL_TIME_START + (worldPosition.x * FALL_TIME_MULTIPLIER);
-            DestroyAfter = Lifetime + 10;
+            m_lifetime = FallTimeStart - (-worldPosition.x * FallTimeMultiplier);
+            m_destroyAfter = m_lifetime + 10;
         }
-        else if (worldPosition.x > 24 )
+        else if (worldPosition.x > 0)
         {
-            worldPosition.x -= 23;
-			Lifetime = FALL_TIME_START + ((rightSideSecondOffset - worldPosition.x) * FALL_TIME_MULTIPLIER);
-            DestroyAfter = Lifetime + 10;
+            m_lifetime = FallTimeStart - (worldPosition.x * FallTimeMultiplier);
+            m_destroyAfter = m_lifetime + 10;
         }
 
-		MaxFallOffset = 0.5f;
-        float rangeAdjustment = Random.Range(0.01f, MaxFallOffset) - MaxFallOffset/2.0f;
+        m_maxFallOffset = 0.5f;
+        float rangeAdjustment = Random.Range(0.01f, m_maxFallOffset) - m_maxFallOffset / 2.0f;
         m_CurrentShakeMag = MinShakeMagnitude;
-        Lifetime += rangeAdjustment;
-        DestroyAfter += rangeAdjustment;
+        m_lifetime += rangeAdjustment;
+        m_destroyAfter += rangeAdjustment;
         m_StartPosition = this.transform.position;
-
-
     }
 
     // Creates a rumbling/shaking effect
-    void Shake ()
+    void Shake()
     {
         Vector3 shakePosition = m_StartPosition;
-        shakePosition.x += Random.Range(0.0f, m_CurrentShakeMag) - m_CurrentShakeMag/2.0f;
-        shakePosition.y += Random.Range(0.0f, m_CurrentShakeMag) - m_CurrentShakeMag/2.0f;
+        shakePosition.x += Random.Range(0.0f, m_CurrentShakeMag) - m_CurrentShakeMag / 2.0f;
+        shakePosition.y += Random.Range(0.0f, m_CurrentShakeMag) - m_CurrentShakeMag / 2.0f;
 
         if (m_CurrentShakeMag < MaxShakeMagnitude) m_CurrentShakeMag += 0.01f * Time.deltaTime;
-        
+
         Vector3 pos = this.transform.position;
         pos.x = shakePosition.x;
         pos.y = shakePosition.y;
         this.transform.position = pos;
     }
 
-	// Update is called once per frame
-	void Update () 
+    // Update is called once per frame
+    void Update()
     {
         m_LifeSpent += Time.deltaTime;
 
-        if (m_LifeSpent > Lifetime - ShakeTime)
+        //// Esnure physics doesn't toss our tiles into the air
+        //if (this.transform.position.z < 0f)
+        //{
+        //    Vector3 pos = this.transform.position;
+        //    //pos.z = 0f;
+        //    this.transform.position = pos;
+        //}
+
+        if (m_LifeSpent > m_lifetime - ShakeTime)
         {
             Shake();
         }
 
-        if (m_LifeSpent > Lifetime)
+        if (m_LifeSpent > m_lifetime)
         {
             this.GetComponent<Rigidbody>().isKinematic = false;
             this.GetComponent<Rigidbody>().WakeUp();
         }
-        if (m_LifeSpent > DestroyAfter)
+        if (m_LifeSpent > m_destroyAfter)
         {
             Destroy(this.gameObject);
         }
-	}
+    }
 }
